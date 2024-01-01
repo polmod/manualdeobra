16434d1c6f0f6d276c43b78550f1d9c73816741b
pol <paolomod3nese@gmail.com>
2024-01-01 20:22:47 +0100

    Add pages

---
+++ script.js
@@ -0,0 +1,10 @@
+    
+var navbarItems = `
+    <ul id="navbar">
+      <li><a href="./index.html">Inicio</a></li>
+      <li><a href="./entrega.html">Entrega</a></li>
+      <li><a href="./libro.html">Libro</a></li>
+      <li><a href="./autor.html">Autor</a></li>
+    </ul>
+`
+document.body.insertAdjacentHTML("afterbegin", navbarItems);
