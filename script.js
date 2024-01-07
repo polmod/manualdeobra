7a614b4c9463d30f79eab6631c08b2dca75ab443
pol <paolomod3nese@gmail.com>
2024-01-07 11:21:26 +0100

    Add responsiveness to nav bar

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
     
 var navbarItems = `
-    <div id="header">
-      <a href="./index.html">
-        <img src="./res/favicon.png" alt="Boceto de un bailejo"></img>
-      </a>  
-      <ul id="navbar">
-        <li><a href="./index.html">Inicio</a></li>
-        <li><a href="./comprar.html">Comprar</a></li>
-        <li><a href="./detalles.html">Detalles</a></li>
-        <li><a href="./autor.html">Autor</a></li>
-      </ul>
-      <div></div>
-    </div>
+<!-- Navigation bar -->
+<header class="header">
+    <!-- Logo -->
+    <a href="./index.html" class="logo">
+      <img src="./res/favicon.png" alt="Boceto de bloque">
+    </a>
+    <!-- Hamburger icon -->
+    <input class="side-menu" type="checkbox" id="side-menu"/>
+    <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+    <!-- Menu -->
+    <nav class="nav">
+        <ul class="menu">
+            <li><a href="./index.html">Inicio</a></li>
+            <li><a href="./comprar.html">Comprar</a></li>
+            <li><a href="./detalles.html">Detalles</a></li>
+            <li><a href="./autor.html">Autor</a></li>
+        </ul>
+    </nav>
+</header>
 `
 document.body.insertAdjacentHTML("afterbegin", navbarItems);
 
